d55a8e92075ce3cc1dab31d9bf66ebe86127dd08
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-03 06:12:08 +0000

    config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// primitive data types: 
+// string :
+console.log("Nitin");
+// number : 
+console.log(345);
+// boolen :
+let accountLogIn = true
+// null > standalone valuve
+// undifined > undifined
+// symbol > unique 
+
+// object 
