f6afb836a12d504d0b0aebd20204261889ffcd97
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-08 10:52:06 +0000

    data types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,12 +1,25 @@
-// primitive data types: 
-// string :
-console.log("Nitin");
-// number : 
-console.log(345);
-// boolen :
-let accountLogIn = true
-// null > standalone valuve
-// undifined > undifined
-// symbol > unique 
+// pritive data type: string, number, boolean, null, symboll, bigInt, undefined
 
-// object 
+let mystring = "This is an eg. of string!"
+let mynumber = 123445
+let myboolean = true
+let mysymbol = Symbol('123')
+let myundifined;
+let mybigInt = 98774848475845748857n
+
+console.log(typeof mystring);
+console.log(typeof mynumber);
+console.log(typeof myboolean);
+console.log(typeof mybigInt);
+console.log(typeof mysymbol);
+
+// non primitive data type: Array, object, function
+
+let myarray = [112, 34, "hello", 34, "world", "i am nitin"]
+let myobj = {
+    name : "nitin",
+    age : 23
+}
+let myfunction = function myfunction() {
+    console.log("Heello Worldd !!")
+}
