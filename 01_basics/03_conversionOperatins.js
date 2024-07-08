c8300288f2e8d9e878eb71178262e170f7b35568
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-08 10:13:38 +0000

    operations

--- 01_basics/03_conversionOperatins.js
+++ 01_basics/03_conversionOperatins.js
@@ -1,7 +1,12 @@
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof(stringNumber));
+// console.log(typeof(stringNumber));
 
 let isBoolen = 0 
 let checker = Boolean(isBoolen);
-console.log(checker);
+// console.log(checker);
+
+let srt1 = "hello "
+let str2 = "Nitin"
+str3 = srt1 + str2;
+console.log(str3)
