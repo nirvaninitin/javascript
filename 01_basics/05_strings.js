09058f244f843db2ea331e875546a6926fe1cc3c
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-13 17:28:46 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+// An better and mordern way of writing strings:
+let name = "Nitin"
+console.log(`hello my name is ${name}. Welcome to my codebase.`);
+// lenght of the string
+let myString = " Hello i am nitin nirvani."
+console.log(myString.length);
+// prototype
+console.log(myString.__proto__);
+// uppercase
+console.log(myString.toUpperCase());
+// lowercase
+console.log(myString.toLowerCase());
+// caracter at position
+console.log(myString.charAt(3));
+// value at index
+console.log(myString.indexOf('n'));
