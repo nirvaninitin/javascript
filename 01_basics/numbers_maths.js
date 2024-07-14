dd237f661a37fed9903ac14ad204797e999c97d1
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-14 12:34:12 +0000

    Numbers And Math

---
+++ 01_basics/numbers_maths.js
@@ -0,0 +1,21 @@
+// ++++++++  numbers  ++++++++++
+// proto-types:
+// // toFixed():
+// const myNumber = 12300000
+console.log(myNumber.toFixed(2));
+// // toPrecision():
+console.log(myNumber.toPrecision());
+// // tolocalestring("en-IN" for indian category)
+console.log(myNumber.toLocaleString('en-IN'));
+
+// ++++++++  Math +++++++
+// rounding of numbers:
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.round(4.3));
+// generate random number:
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
