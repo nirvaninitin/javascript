d01deb33b8b26106fecf0de8d7f56bf1e2c09ec0
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-15 15:26:17 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+// declaration of obj
+
+const mySym = Symbol("key1");
+const myObj = {
+    name : "nitin",
+    age : 19,
+    email:"nitin@google.comm",
+    [mySym]: "mykey1"
+}
+
+// the correct way of asseccing the obj
+// console.log(myObj['name']);
+
+// how to get smybol value by obj
+// console.log(myObj[mySym]);
+
+// to change the the value on the object 
+myObj.email = "nitinnirvani@google.com";
+// console.log(myObj);
+
+// to freeze the object 
+// Object.freeze(myObj)
+myObj.email = "nitin@wipro.com";
+// console.log(myObj);
+
+// adding function 
+
+myObj.greeting = function(){
+    console.log(`hello user ${this.name}`);
+}
+console.log(myObj.greeting());
+
+
