b5ff3d618fad388857ca91cbabdd0ccad6742144
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-16 13:26:44 +0000

    object de-structure and json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,19 @@
+const course = {
+    name : "Js in hindi",
+    price : 99,
+    instructor : 'Nitin',
+    time : '2 months'
+}
+
+const {instructor: sir} = course;
+
+// console.log(instructor);
+console.log(sir);
+
+
+// json
+{
+    "name":"Nitin",
+    "age":19,
+    "work":"web Developer"
+}
