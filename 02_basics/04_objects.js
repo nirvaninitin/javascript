8de53b77d64f03e2857119c80eae41bb9c2f383e
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-16 09:42:28 +0000

    more in objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,3 +27,11 @@ const obj1 = {1:"a",2:"b"};
 const obj2 = {3:"a",4:"b"};
 const obj3 = Object.assign({}, obj1, obj2);
 console.log(obj3);
+
+// another method to do the same:
+const obj4 = {...obj1,...obj2};
+console.log(obj4);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
