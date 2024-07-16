72ce7c1e62fa5b867f5f24a89f6a8a15d5851a34
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-16 08:42:32 +0000

    more in objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,29 @@
+// singleton objects 
+ 
+const user = {}
+
+user.name = "nitin";
+user.email = "some@gmail.com"
+user.isLogedIn = false
+
+console.log(user);
+
+const anotherUser = {
+    email: "another@gmail.com",
+    fullname:{
+        firstName:{
+            name:"nitin"
+        },
+        lastname:{
+            surname:"Nirvani"
+        }
+    }
+}
+console.log(anotherUser.fullname.firstName.name);
+
+// merging / combining the objs
+
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"a",4:"b"};
+const obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3);
