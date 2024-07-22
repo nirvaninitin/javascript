e58c877ff85066c846f63f07ec18e57d46ddebba
nirvaninitin <154454189+nirvaninitin@users.noreply.github.com>
2024-07-22 16:00:41 +0000

    while and do while loop

---
+++ 05_loops/while.js
@@ -0,0 +1,18 @@
+let index = 0
+// while (index <= 10 ) {
+//     console.log(`the value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray  = ["superman", "batman","flash"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(`The value of array is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let one = 0
+do {
+    console.log(`the score is ${one}`);
+    one ++
+} while (one <= 5);
